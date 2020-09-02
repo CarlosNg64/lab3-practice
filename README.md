25dc3576839876468c01139e3a2a1215bc0dea19
CarlosNg <carlos.iglesias@upr.edu>
2020-09-02 19:00:39 -0400

    HEY ther

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # lab3-practice
 Hello hell.
+Hey
---
+++ slow.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    string lol;
+    string bruh = "not a bruh moment";
+    auto huh = 12.23457;
+    cout << "this is " << bruh << " but " << huh << " times smaller"<< endl;
+    getline(cin, lol);
+    cout << "idk "<< lol;
+    
+    
+    return 0;
+}
